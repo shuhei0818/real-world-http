f9b1d31cab16a203b36f7b2d715f0f3c002de2ac
shuhei0818 <shuhei.kt@gmail.com>
2021-12-01 19:48:58 +0900

    Update 3.4 Client Get

--- 3-4-client-get/main.go
+++ 3-4-client-get/main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net/http"
 	"os"
-	"strings"
+	"realhttpworld"
 )
 
 func main() {
-	res, err := http.Get("http://localhost:8888")
+	res, err := http.Get("http://localhost:8888/")
 
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
@@ -17,22 +16,6 @@ func main() {
 	}
 	defer res.Body.Close()
 
-	b, err := io.ReadAll(res.Body)
-	if err != nil {
-		fmt.Fprint(os.Stderr, err)
-		os.Exit(1)
-	}
-
-	fmt.Printf("%s %s", res.Proto, res.Status)
-
-	fmt.Println()
-
-	for k, v := range res.Header {
-		fmt.Printf("%s: %s\n", k, strings.Join(v, ";"))
-	}
-
-	fmt.Println()
-
-	fmt.Println(string(b))
+	realhttpworld.Write(res)
 
 }
