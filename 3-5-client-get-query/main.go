d6bc892efbd1ad7a6a482edcf930534574ce5c2b
shuhei0818 <shuhei.kt@gmail.com>
2021-12-01 19:49:18 +0900

    Add 3.5 Client Get Query

---
+++ 3-5-client-get-query/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+func main() {
+
+	values := url.Values{
+		"query": {"hello world"},
+	}
+
+	r, err := http.Get("http://localhost:8888" + "?" + values.Encode())
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	defer r.Body.Close()
+
+}
