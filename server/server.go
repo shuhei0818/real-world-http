35530c17a8a19cf6b86b5cee83ec742993ed8a99
shuhei0818 <shuhei.kt@gmail.com>
2021-12-01 19:48:25 +0900

    Update server

--- server/server.go
+++ server/server.go
@@ -2,27 +2,12 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net/http"
-	"strings"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 
-	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL.Path, r.Proto)
-	for k, v := range r.Header {
-		fmt.Fprintf(w, "%s: %s\n", k, strings.Join(v, ";"))
-	}
-
-	fmt.Fprintf(w, "\n")
-
-	rc := r.Body
-	defer rc.Close()
-	b, _ := io.ReadAll(rc)
-
-	if b != nil {
-		fmt.Fprintf(w, "%s\n", string(b))
-	}
+	fmt.Fprint(w, "Hello World\n")
 }
 
 func main() {
---
+++ writer.go
@@ -0,0 +1,42 @@
+package realhttpworld
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+)
+
+func Write(resp *http.Response) {
+	fmt.Println("---- Request ----")
+	fmt.Printf("%s %s %s\n", resp.Request.Method, resp.Request.URL.Path, resp.Request.Proto)
+
+	for k, v := range resp.Request.Header {
+		fmt.Printf("%s: %s\n", k, strings.Join(v, ";"))
+	}
+
+	fmt.Println()
+
+	var qb []byte
+	if resp.Request.Body != nil {
+		qb, _ = io.ReadAll(resp.Request.Body)
+	}
+
+	fmt.Println(string(qb))
+
+	fmt.Println("---- Response ----")
+
+	fmt.Printf("%s %s", resp.Proto, resp.Status)
+
+	fmt.Println()
+
+	for k, v := range resp.Header {
+		fmt.Printf("%s: %s\n", k, strings.Join(v, ";"))
+	}
+
+	fmt.Println()
+
+	b, _ := io.ReadAll(resp.Body)
+
+	fmt.Println(string(b))
+}
