20f383bfd1f2bbbbc182f5c2be6fb2b1f2001512
shuhei0818 <shuhei.kt@gmail.com>
2021-12-03 22:27:29 +0900

    Add 3-7

--- 3-7-client-post-x-www-form-urlencoded/main.go
+++ 3-7-client-post-x-www-form-urlencoded/main.go
@@ -9,9 +9,10 @@ import (
 func main() {
 
 	values := url.Values{
-		"test": {"value"},
+		"test": {"hello world"},
 	}
 
+	// PostForm RFC3986でエンコード
 	r, err := http.PostForm("http://localhost:8888/", values)
 	if err != nil {
 		panic(1)
